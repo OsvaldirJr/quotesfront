e12682e44fe8940a7d6ea35fcb6eb5865d450b22
Osvaldir Dias da Silva Junior <diasosvaldirjr@gmail.com>
2025-04-01 22:29:56 -0300

    :construction: creates pages and add angular material into the project

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,7 +4,9 @@ import { MainRoutingModule } from './main-routing.module';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/component/dashboard.component';
 import { NavbarComponent } from './dashboard/components/navbar/navbar.component';
-
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 @NgModule({
   declarations: [
     DashboardComponent,
@@ -12,7 +14,10 @@ import { NavbarComponent } from './dashboard/components/navbar/navbar.component'
   ],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [
     provideClientHydration()
